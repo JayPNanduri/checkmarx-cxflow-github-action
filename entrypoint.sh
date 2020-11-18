ca804e6037b3fa2335b15de44bc64e75dedad1ed
Jayendra Nanduri <jay.nanduri@checkmarx.com>
2020-11-18 12:41:43 -0500

    Changed entrypoint and project

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -jar /app/cx-flow.jar --spring.profiles.active=sast --scan --github.api-url="${GITHUB_API_URL}/repos/" --cx-team="${TEAM}" --cx-project="${PROJECT}" --app="${APP}" --f=. ${CXFLOW_PARAMS}
+java -jar /app/cx-flow.jar --spring.profiles.active=sast --project --github.api-url="${GITHUB_API_URL}/repos/" --cx-team="${TEAM}" --cx-project="${PROJECT}" --app="${APP}" --f=. ${CXFLOW_PARAMS}
